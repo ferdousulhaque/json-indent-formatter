dd97a26e98351c0cf277e8dea67601d1a50d22ba
A S Md Ferdousul Haque <ferdousul.haque@gmail.com>
2021-03-05 15:09:41 +0600

    readme.md update and file path update

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import json
 
 
-def json_indent(filepath):
-    handler = read_a_file(filepath)
-    filename = filename_only(filepath)
-    path = path_only(filepath)
+def json_indent(file_path):
+    handler = read_a_file(file_path)
+    filename = filename_only(file_path)
+    path = path_only(file_path)
     file = open(path + filename + "_formatted.json", "w")
     file.write(json.dumps(json.load(handler), sort_keys=True, indent=4))
     file.close()
 
 
-def read_a_file(filepath):
-    content = open(filepath, "r")
+def read_a_file(file_path):
+    content = open(file_path, "r")
     return content
 
 
@@ -20,15 +20,15 @@ def take_file_location_input():
     return x
 
 
-def filename_only(filepath):
-    split_arr = filepath.split("/")
+def filename_only(file_path):
+    split_arr = file_path.split("/")
     count = len(split_arr)
     filename = split_arr[count-1].split(".")
     return filename[0]
 
 
-def path_only(filepath):
-    return '/'.join(filepath.split('/')[0:-1])+'/'
+def path_only(file_path):
+    return '/'.join(file_path.split('/')[0:-1])+'/'
 
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -2,3 +2,15 @@
 As the name describes, this is a simple JSON Indent Formatter for large files, which are not able to beautify from the IDE.
 
 Idea was when I got a 100mb JSON file and tried to open it in an IDE. I was not able to format the JSON to see it as a clear view, as the json was only 1 liner.
+
+## Run Script
+If you have only one python version installed. Then use the following command to run.
+```
+python main.py
+```
+Next it will ask you the filepath. Share the full path to the file.
+```
+Provide file location:/home/ferdous/filename.json
+```
+It will then ask for the formatted json file name. By default, it is set as the current filename added _formatted at the end.
+
